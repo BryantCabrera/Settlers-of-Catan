533bbeafab2ce9e54bfb201dac6e9f02dd1f4fd5
Bryant Cabrera <bryantcabrera@Bryants-MacBook-Pro.local>
2019-01-04 14:24:11 -0800

    Second commit test.

---
+++ version1 copy/js/app.js
@@ -0,0 +1,55 @@
+//JavaScript
+/*
+1. add event listeners to all road areas, settlement & port areas, buttons, robber, hexes, and cards
+2. initialize the state
+    2a. define players (#, name, etc.) 
+    2b. decide turn order
+    2c. set player victory points to 0
+    2d. refill deck
+3. call render
+4. initial turn
+    4a. players click board to place initial settlements and roads
+    4b. store settlement/port areas, road areas taken
+    4c. distribute initial hand based on initial settlement placement
+    4d. update remaining cards in deck
+5. prompt dice roll
+    5a. store dice roll value
+    5b. check dice roll value
+            5bi. if 7
+                5bi.i. check player's hand amounts.
+                    5bi.i.i. if >7, prompt to discard Math.floor(hand/2)
+                5bi.ii. display areas where current player can move robber
+                5bi.iii. prompt who current player can steal from
+                5bi.iv. remove chosen card from opponent's hand and place it into player's hand.
+                5bi.v. update class of settlements on robbed land to "robbed"
+            5bii. if not 7 
+                5bii.i. check value against stored settlement/port area values
+                5bii.ii. distribute resource cards accordingly
+    5d. update remaining cards in deck
+    5e. prompt user for build, purchase, trade, or end turn
+        5ei. if build
+            5ei.i. check player's current resources
+            5ei.ii. display build options
+            5ei.iii. on click, display possible areas to build
+            5ei.iv. store player's choice
+            5ei.v. update player resources
+        5eii. if purchase
+            5eii.i. check player's current resources
+            5eii.ii. display possible purchase options
+            5eii.iii. update player's resources
+        5eiii. if trade
+            5eiii.i. display trade rates
+            5eiii.ii. store player's choice
+            5eiii.iii. update player's resources & remaining deck
+        5eiv. if end turn, continue to step 5f.
+    5f.check special condition fulfillment
+        5fi. if yes allot victory points & distribute cards accordingly
+        5fii. if no continue to 5g
+    5g. allot victory points accordingly
+    5h. calculate if there is winner
+        5hi. if yes, find winner & end game
+        5hii. if no continute
+6. change player turn
+    6a. repeat step 5
+
+*/

d8419d6cd06961646acf5558b56a755cf2297911
Bryant Cabrera <bryantcabrera@Bryants-MacBook-Pro.local>
2019-01-04 19:05:20 -0800

    Adds event listener changes.

--- version1/js/app.js
+++ version1/js/app.js
@@ -56,8 +56,8 @@
 
 let turn = 1;
 $('.hexes').on('click', function (e) {
-    console.log($(e.target).attr('class'))
-    if ($(e.target).attr("class") !== "row" || $(e.target).attr("class") !== "hex") {
+    console.log($(e.target).attr('class'));
+    if ($(e.target).attr('class') !== "row" || $(e.target).attr('class') !== "hex") {
       $(e.target)
         .css("background-color", `var(--player${turn}-color)`)
         .css("opacity", "1")
